01bb66ea7c42f5bcdae4b6a8203804a84776a69d
Daman Mulye <daman_mulye@hotmail.com>
2026-09-08 16:16:37 -0700

    rpc: support host bind addresses and read-only virtio-fs shares

--- vmm_tests/vmm_tests/tests/tests/ttrpc.rs
+++ vmm_tests/vmm_tests/tests/tests/ttrpc.rs
@@ -471,6 +471,7 @@ async fn test_ttrpc_interface_inner(
                             virtiofs_config: vec![vmservice::VirtioFs {
                                 tag: "testfs".to_string(),
                                 root_path: virtiofs_root.to_string_lossy().into(),
+                                read_only: false,
                             }],
                             // A SCSI controller keeps a request channel
                             // alive for the lifetime of the VM, which used
@@ -527,6 +528,7 @@ async fn test_ttrpc_interface_inner(
                                             host_port: 8080,
                                             guest_port: 80,
                                             protocol: 99,
+                                            host_address: String::new(),
                                         }],
                                     },
                                 )),
@@ -582,6 +584,7 @@ async fn test_ttrpc_interface_inner(
                             vmservice::VirtioFs {
                                 tag: "hotplugfs".to_string(),
                                 root_path: hotplug_virtiofs_root.to_string_lossy().into(),
+                                read_only: false,
                             },
                         ))),
                     },
@@ -1094,6 +1097,7 @@ fn virtio_fs_vpci_request(
             vmservice::VirtioFs {
                 tag: tag.to_string(),
                 root_path: root_path.to_string_lossy().into_owned(),
+                read_only: false,
             },
         ))),
     }
